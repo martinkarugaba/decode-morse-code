481a0985af494d2171c7fb7e24198969e3ec1cfe
Samuel T. Boateng <samueltakyiboateng@gmail.com>
2023-11-01 10:41:37 -0500

    Add decode word and message methods

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -12,7 +12,13 @@ class MorseCodeDecoder
   def self.decode_char(morse_char)
     MORSE_CODE[morse_char] || ''
   end
-
+  def self.decode_word(morse_word)   
+     morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')  
+    end  
+    def self.decode(message)    
+      words = message.split('   ')    
+      decoded_message = words.map { |morse_word| decode_word(morse_word) }.join(' ')  
+  end
 end
 
 example_message="-- -.--   -. .- -- ."
