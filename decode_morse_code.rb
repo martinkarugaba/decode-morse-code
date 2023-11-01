ccd89f7d2e141281688b2c4b6993a0852b795bd3
Martin Karugaba <martinkarugaba21@gmail.com>
2023-11-01 19:14:00 +0300

    Fix linter errors

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# MorseCodeDecoder decodes morse messages
 class MorseCodeDecoder
   MORSE_CODE = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
@@ -7,24 +10,22 @@ class MorseCodeDecoder
     '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
     '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
     '-.--' => 'Y', '--..' => 'Z'
-  }
+  }.freeze
 
   def self.decode_char(morse_char)
     MORSE_CODE[morse_char] || ''
   end
 
   def self.decode_word(morse_word)
-     morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')
+    morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')
   end
 
-  def self.decode(message) 
-    words = message.split('   ')   
-    decoded_message = words.map { |morse_word| decode_word(morse_word) }.join(' ')  
+  def self.decode(message)
+    words = message.split('   ')
+    words.map { |morse_word| decode_word(morse_word) }.join(' ')
   end
 end
 
-example_message="-- -.--   -. .- -- ."
-morse_message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+morse_message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_message = MorseCodeDecoder.decode(morse_message)
 puts decoded_message
-
